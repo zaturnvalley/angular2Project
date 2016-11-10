f3ef883063dd43c4f6a276a671a48f6a1047775d
zaturnvalley <zacharycmason@gmail.com>
2016-11-10 15:30:37 -0800

    added courses component

--- app/courses.component.js
+++ app/courses.component.js
@@ -11,27 +11,27 @@ System.register(['angular2/core'], function(exports_1, context_1) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
     var core_1;
-    var AppComponent;
+    var CoursesComponent;
     return {
         setters:[
             function (core_1_1) {
                 core_1 = core_1_1;
             }],
         execute: function() {
-            AppComponent = (function () {
-                function AppComponent() {
+            CoursesComponent = (function () {
+                function CoursesComponent() {
                 }
-                AppComponent = __decorate([
+                CoursesComponent = __decorate([
                     core_1.Component({
-                        selector: 'my-app',
-                        template: '<h1>Hello Angular</h1>'
+                        selector: 'courses',
+                        template: '<h2>Courses</h2>'
                     }), 
                     __metadata('design:paramtypes', [])
-                ], AppComponent);
-                return AppComponent;
+                ], CoursesComponent);
+                return CoursesComponent;
             }());
-            exports_1("AppComponent", AppComponent);
+            exports_1("CoursesComponent", CoursesComponent);
         }
     }
 });
-//# sourceMappingURL=app.component.js.map
+//# sourceMappingURL=courses.component.js.map
---
+++ app/courses.component.ts
@@ -0,0 +1,9 @@
+import {Component} from 'angular2/core'
+
+@Component({
+  selector: 'courses',
+  template: '<h2>Courses</h2>'
+})
+export class CoursesComponent {
+  
+}
